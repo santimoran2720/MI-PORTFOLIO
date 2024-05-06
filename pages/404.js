6fd015a0ac47dbcb7a4435f95c88ccdaa407ff33
santimoran2720 <moran3490@gmail.com>
2024-05-06 19:52:00 -0300

    First commit

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+const custom404 = () => (
+  <Layout title="Page Not Found">
+    <div className="text-center">
+      <h1 className="display-1">404</h1>
+      <p>
+        This page does not exists. Please return to <Link href="/">Home</Link>
+      </p>
+    </div>
+  </Layout>
+);
+
+export default custom404;
