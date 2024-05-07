0828d582dec54c9c98e78751f5ebeaab1392bbcc
santimoran2720 <moran3490@gmail.com>
2024-05-07 11:04:46 -0300

    Segunda prueba calc

--- profile.js
+++ profile.js
@@ -54,10 +54,11 @@ export const experiences = [
 
 export const projects = [
   {
-    name: " Website 1",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
-    image: "portfolio1.jpeg",
+    
+    "title": "Calculadora",
+    "text": "Uno de mis primeras pruebas de autodidacta fue esta calculadora que aunque sea muy basica fue lo que me ayudo a romper el hielo con la programacion",
+    "link": "https://calculadora-ilonnxnko-morans-teams-projects.vercel.app/",
+    "image": "calculator.png"
   },
   {
     name: " Website 2",
