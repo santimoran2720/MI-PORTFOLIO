1e43e4cc5f8b135fd28d7c29874a32835a3f9e62
santimoran2720 <moran3490@gmail.com>
2024-05-07 11:07:02 -0300

    calc

--- profile.js
+++ profile.js
@@ -54,11 +54,10 @@ export const experiences = [
 
 export const projects = [
   {
-    
-    "title": "Calculadora",
+    "title": "file:///home/santiago/Desktop/Calduladora.js/index.html",
     "text": "Uno de mis primeras pruebas de autodidacta fue esta calculadora que aunque sea muy basica fue lo que me ayudo a romper el hielo con la programacion",
     "link": "https://calculadora-ilonnxnko-morans-teams-projects.vercel.app/",
-    "image": "calculator.png"
+    "image": "./public/calculator.png"
   },
   {
     name: " Website 2",
