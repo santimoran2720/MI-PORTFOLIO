614e7153ef224c9cc8a0a76ff4f7dfd8eb7a866b
santimoran2720 <moran3490@gmail.com>
2024-05-22 07:35:11 -0300

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 // importing bootswatch
 import 'bootswatch/dist/cosmo/bootstrap.min.css'
 import 'animate.css/animate.min.css'
-import '../global.css'
+
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
